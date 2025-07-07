b542f11c5c2a970ef0c6bda3015b2f8530d83f85
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-07 12:17:00 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,24 +1,27 @@
-from sqlalchemy import Column, Integer, String, Date, Text, ForeignKey, TIMESTAMP
+from sqlalchemy import Column, Integer, String, Date, Text, ForeignKey, TIMESTAMP, func
 from backend.database import Base
 
 class EmployeeInfo(Base):
     __tablename__ = "employee_info"
+    
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
-    date_of_birth = Column(Date)
-    address = Column(Text)
-    contact_number = Column(String(15))
-    pan_number = Column(String(10))
-    aadhar_number = Column(String(12))
-    created_at = Column(TIMESTAMP)
+    name = Column(String(100), nullable=False)
+    date_of_birth = Column(Date, nullable=False)
+    address = Column(Text, nullable=False)
+    contact_number = Column(String(15), nullable=False, unique=True)
+    pan_number = Column(String(10), nullable=False, unique=True)
+    aadhar_number = Column(String(12), nullable=False, unique=True)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+
 
 class EmployeeDocuments(Base):
     __tablename__ = "employee_documents"
+    
     id = Column(Integer, primary_key=True, index=True)
-    employee_id = Column(Integer, ForeignKey("employee_info.id", ondelete="CASCADE"))
-    resume = Column(Text)
-    educational_certificates = Column(Text)
-    offer_letters = Column(Text)
-    pan_card = Column(Text)
-    aadhar_card = Column(Text)
-    form_16_or_it_returns = Column(Text)
+    employee_id = Column(Integer, ForeignKey("employee_info.id", ondelete="CASCADE"), nullable=False)
+    resume = Column(Text, nullable=False)
+    educational_certificates = Column(Text, nullable=False)
+    offer_letters = Column(Text, nullable=False)
+    pan_card = Column(Text, nullable=False)
+    aadhar_card = Column(Text, nullable=False)
+    form_16_or_it_returns = Column(Text, nullable=False)
