dfa61277b4270a4487b088a5369207dcb097dec7
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-07 12:08:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from backend.database import Base, engine
-from backend.routers import employee, documents, auth  # new
+from backend.routers import employee, documents, auth, files # new
 from backend.admin import main as admin_main  # optional, if you have admin-specific routes
 
 Base.metadata.create_all(bind=engine)
@@ -16,3 +16,4 @@ app.include_router(auth.router)
 app.include_router(employee.router)
 app.include_router(documents.router)
 app.include_router(admin_main.router)  # optional
+app.include_router(files.router)
