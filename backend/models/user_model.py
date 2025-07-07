d4f30278d0479e502bd2cee8a96899914fef94aa
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-07 12:13:51 +0530

    Update user_model.py

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,10 +1,15 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, constr
+from enum import Enum
+
+class UserRole(str, Enum):
+    admin = "admin"
+    employee = "employee"
 
 class UserCreate(BaseModel):
     email: EmailStr
-    password: str
-    role: str  # "admin" or "employee"
+    password: constr(min_length=8, max_length=64, regex=r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]+$')
+    role: UserRole
 
 class UserLogin(BaseModel):
     email: EmailStr
-    password: str
+    password: constr(min_length=8, max_length=64)
