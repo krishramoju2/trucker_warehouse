7cf3c1fe27be9c670d15e4ac37364e3791bd0b5e
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-11 11:09:16 +0530

    Create search.py

---
+++ backend/routers/search.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from backend.utils.semantic_index import semantic_search
+from backend.database import SessionLocal
+from backend.models import EmployeeInfo
+
+router = APIRouter()
+
+@router.get("/semantic-search")
+def semantic_search_api(query: str):
+    emp_ids = semantic_search(query)
+    db = SessionLocal()
+    results = db.query(EmployeeInfo).filter(EmployeeInfo.id.in_(emp_ids)).all()
+    return results
