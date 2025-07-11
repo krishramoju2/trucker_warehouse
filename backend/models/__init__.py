1c16b55322c13936d067c9d627893dd2be45816d
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-11 12:55:11 +0530

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,5 +1,1 @@
 from .user_model import UserCreate, UserLogin
-from .employee_model import EmployeeInfo
-from .document_model import EmployeeDocuments
-
-__all__ = ["UserCreate", "UserLogin", "EmployeeInfo", "EmployeeDocuments"]
