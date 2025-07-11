6525706ee7ff6117e382a7c659bfd5c72b8f5974
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-11 12:42:38 +0530

    Create __init__.py

---
+++ backend/models/__init__.py
@@ -0,0 +1,5 @@
+from .user_model import UserCreate, UserLogin
+from .employee_model import EmployeeInfo
+from .document_model import EmployeeDocuments
+
+__all__ = ["UserCreate", "UserLogin", "EmployeeInfo", "EmployeeDocuments"]
