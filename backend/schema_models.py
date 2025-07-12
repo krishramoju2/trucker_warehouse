b6077f98483bb29ab5d5ef28b92d221e773e27f3
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-12 13:20:28 +0530

    Update schema_models.py

--- backend/schema_models.py
+++ backend/schema_models.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, String, Date, Text, ForeignKey, TIMESTAMP, func
 from backend.database import Base
-
+from sqlalchemy.orm import relationship
 
 class EmployeeInfo(Base):
     __tablename__ = "employee_info"
-    from sqlalchemy.orm import relationship
+    
     documents = relationship("EmployeeDocuments", backref="employee", cascade="all, delete")
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
